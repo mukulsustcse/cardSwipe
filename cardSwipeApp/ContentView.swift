60679a720081a01322b1c2b0098feb8b7dba3004
M. A. Alim Mukul <mukul.cse2010@gmail.com>
2022-09-03 13:56:28 +0200

    * Make a Rectangular black card adjusted to the screen using GeometryReader * Number with a circle is printed over the card

--- cardSwipeApp/ContentView.swift
+++ cardSwipeApp/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        GeometryReader {
+            geo in
+            
+            TabView {
+                
+                ForEach(1...10, id: \.self) {
+                    index in
+                    
+                    ZStack {
+                        Rectangle()
+                        
+                        Image(systemName: String(index)+".circle")
+                            .resizable()
+                            .foregroundColor(.white)
+                            .frame(width: 150, height: 150, alignment: .center)
+                            
+                            
+                    }
+                    .frame(width: geo.size.width-40, height: geo.size.height-100, alignment: .center)
+                    .cornerRadius(20)
+                    .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 5), radius: 10, x: 5, y: 5)
+                }
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+        }
     }
 }
 
