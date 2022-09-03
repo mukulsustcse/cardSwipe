7b8a942be4f0f942a2b33f93f623e9115cefd64b
M. A. Alim Mukul <mukul.cse2010@gmail.com>
2022-09-03 13:16:16 +0200

    Initial Commit

---
+++ cardSwipeApp/cardSwipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardSwipeAppApp.swift
+//  cardSwipeApp
+//
+//  Created by M. A. Alim Mukul on 03.09.22.
+//
+
+import SwiftUI
+
+@main
+struct cardSwipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
