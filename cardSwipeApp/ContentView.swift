f0b70dcf1b72bac8cc7f9e27a0974e8d2b7b1976
M. A. Alim Mukul <mukul.cse2010@gmail.com>
2022-09-03 14:25:51 +0200

    Change random Color every time with every card

--- cardSwipeApp/ContentView.swift
+++ cardSwipeApp/ContentView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     
+    var cardColor : Color {
+        return  Color(.sRGB, red: Double.random(in: 0..<1), green: Double.random(in: 0..<1), blue: Double.random(in: 0..<1), opacity: 1)
+    }
+    
     var body: some View {
         
         GeometryReader {
@@ -16,23 +20,25 @@ struct ContentView: View {
             
             TabView {
                 
-                ForEach(1...10, id: \.self) {
+                ForEach(1...50, id: \.self) {
                     index in
                     
                     ZStack {
                         Rectangle()
+                            .foregroundColor(cardColor)
                         
                         Image(systemName: String(index)+".circle")
                             .resizable()
-                            .foregroundColor(.white)
+                            .foregroundColor(.black)
                             .frame(width: 150, height: 150, alignment: .center)
                             
                             
                     }
-                    .frame(width: geo.size.width-40, height: geo.size.height-100, alignment: .center)
+                    .frame(width: max(geo.size.width-45, 100), height: max(geo.size.height-110, 100))
                     .cornerRadius(20)
                     .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 5), radius: 10, x: 5, y: 5)
                 }
+                
             }
             .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
             .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
